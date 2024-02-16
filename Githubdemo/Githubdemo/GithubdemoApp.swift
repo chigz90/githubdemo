77049a0fb7fe5b0aff7bc555685ba762764b1d50
Chirag Tailor <chirag.tailor@thecoventry.co.uk>
2024-02-16 15:39:32 +0000

    Inital Commit

---
+++ Githubdemo/Githubdemo/GithubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubdemoApp.swift
+//  Githubdemo
+//
+//  Created by Chirag Tailor on 16/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
